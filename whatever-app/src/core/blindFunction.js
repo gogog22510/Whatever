0b4493b74d8268c9e3a9d477fcbc50d95c503fb7
h79822 <h79822@gmail.com>
2019-11-10 12:21:56 -0500

    123

--- whatever-app/src/core/blindFunction.js
+++ whatever-app/src/core/blindFunction.js
@@ -1,7 +1,7 @@
 const blindFunction = (count) => {
     //50 + x^2+0.9487x
     const blindRate = 50 + count * count + 0.9487 * count;
-    return blindRate;
+    return Math.max(blindRate, 0);
 };
 
 export default blindFunction
