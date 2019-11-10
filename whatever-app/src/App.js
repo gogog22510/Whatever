ba933623c322baa03ea0f5137a327527677bab6b
charles <gogog22510@gmail.com>
2019-11-10 10:37:37 -0500

    test

--- whatever-app/src/App.js
+++ whatever-app/src/App.js
@@ -3,17 +3,34 @@ import Box from '@material-ui/core/Box';
 import {Route, Switch} from "react-router-dom";
 import Sample from "./component/Sample";
 import SignIn from "./component/SignIn";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import MainPage from "./component/MainPage";
 
-export default function App() {
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="/">
+                Whatever.inc
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+export default function App(props) {
   return (
     <main>
-      <Box my={4}>
-          <Switch>
-              <Route path="/" exact component={Sample} />
-              <Route path="/login" component={SignIn} />
-              <Route path="/" component={Sample} />
-          </Switch>
-      </Box>
+        <Switch>
+            <Route path="/" exact component={() => <MainPage cookies={props.cookies} />} />
+            <Route path="/login" component={() => <SignIn cookies={props.cookies} />} />
+            <Route path="/" component={Sample} />
+        </Switch>
+        <Box mt={8}>
+            <Copyright />
+        </Box>
     </main>
   );
 }
